1101327528bb394484bb8f0bad4c5624a0b22c5c
Alexandr <sa.brench@gmail.com>
2023-12-21 21:08:19 +0300

    Lab2

---
+++ src/bsu/rfe/java/group6/lab1/Brenchaninov/varA/Main.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group6.lab1.Brenchaninov.varA;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainFrame calc = new MainFrame();
+
+        calc.setVisible(true);
+
+
+    }
+}
